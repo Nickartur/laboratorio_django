d4e210508e462569887b3100c5fd5238a1a14eb4
Nickartur <ncarvajalguzman@gmail.com>
2024-12-31 17:48:27 -0300

    Modificacion Clases en Models
    se agregan modificaciones a models, nuevas caracteristicas

--- laboratorio/models.py
+++ laboratorio/models.py
@@ -3,15 +3,25 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Laboratorio(models.Model):
-    nombre = models.CharField(max_length=128)
-
+    nombre = models.CharField(max_length=128) #,verbose_name="Nombres")
+    ciudad = models.CharField(max_length=128, null=True, blank=True) #,verbose_name="Ciudad")
+    pais = models.CharField(max_length=128, null=True, blank=True) #,verbose_name="Pais")
+    def __str__(self):
+        return self.nombre
+    
 class DirectorGeneral(models.Model):
     nombre = models.CharField(max_length=128)
     laboratorio = models.OneToOneField(Laboratorio, on_delete=models.PROTECT)
-
+    especialidad = models.CharField(max_length=128, null=True, blank=True)
+    def __str__(self):
+        return self.nombre
+       
 class Producto(models.Model):
     nombre = models.CharField(max_length=128)
     laboratorio = models.ForeignKey(Laboratorio, on_delete=models.PROTECT)
     f_fabricacion = models.DateField(default='2015-01-01')
     p_costo = models.DecimalField(max_digits=10, decimal_places=2)
     p_venta = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return self.nombre
+    
